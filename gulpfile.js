a4cd25b62b64786a63f69ce8115939bf23768dd7
kM <chamajiuxi@126.com>
2015-12-08 22:05:57 -0500

    v1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    del = require('del');
+    rename = require('gulp-rename');
+
+gulp.task('scripts', function() {
+  return gulp.src('src/*.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/'], cb);
+});
+
+gulp.task('default', ['scripts']);
